135b878c637ff467da061a3ea30e99355972f376
Dina Filippova <dinafilippova@gmail.com>
2022-12-02 20:17:26 +0700

    Unfinished magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,15 @@
+﻿int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 30;
+int yc = 60;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
