c37811426a514627d56ff5008badad5ebf26a4b5
Dina Filippova <dinafilippova@gmail.com>
2022-12-02 19:03:53 +0700

    toLower

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "дина")
+{
+    Console.WriteLine("Ура, это же Дина!!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
+
