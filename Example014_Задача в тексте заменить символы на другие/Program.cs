a54d4403c37d9f07b211600b787672adaab1c2ce
Dina Filippova <dinafilippova@gmail.com>
2022-12-09 10:50:04 +0700

    Замена букв в тексте с помощью метода

---
+++ Example014_Задача в тексте заменить символы на другие/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля";
+
+string Replace(string text, char oldValue, char newValue)
+{
+string result = String.Empty;
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+if (text[i] == oldValue) result = result + $"{newValue}";
+else result = result + $"{text[i]}";
+}
+return result;
+}
+
+string newtext = Replace(text, ' ', '|');
+Console.WriteLine(newtext);
+
+string newtext1 = Replace(text, 'к', 'К');
+Console.WriteLine(newtext1);
