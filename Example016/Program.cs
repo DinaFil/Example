2111ccc87ea9eea87d55a80667f2658a36968498
Dina Filippova <dinafilippova@gmail.com>
2022-12-13 13:18:17 +0700

    Двумерныемассивы

---
+++ Example016/Program.cs
@@ -0,0 +1,102 @@
+﻿// string[,] table = new string [2,5];
+//table[0, 0] - первый элемент; 
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4]
+// table[1, 4] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+// for (int columns = 0; columns < 5; columns++)
+// Console.WriteLine($"{table[rows, columns]}-");
+// }
+
+int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+// void FillArray(int[,] matr)
+// {
+// for (int i = 0; i < matr.GetLength(0); i++)
+// {
+// for (int j = 0; j < matr.GetLength(1); j++)
+// matr[i, j] = new Random().Next(1, 10);
+// }
+// }
+
+
+// void PrintArray(int[,] image)
+// {
+// for (int i = 0; i < matr.GetLength(0); i++)
+// {
+// for (int j = 0; j < matr.GetLength(1); j++)
+// Console.Write($"{matr[i, j]}");
+// Console.WriteLine();
+// }
+// }
+
+// int[,] matrix = new int [3, 4];
+// PrintArray(matrix);
+// Console.WriteLine();
+// FillArray(matrix);
+// PrintArray(matrix);
+
+void PrintImage(int[,] image)
+{
+ for (int i = 0; i < image.GetLength(0); i++)
+ {
+ for (int j = 0; j < image.GetLength(1); j++)
+ {
+ if(image[i,j] == 0) Console.Write(" ");
+ else Console.Write("+");
+ }
+ Console.WriteLine();
+ }
+}
+PrintImage(pic);
+
+void PrintImage(int[,] image)
+{
+ for (int i = 0; i < image.GetLength(0); i++)
+ {
+ for (int j = 0; j < image.GetLength(1); j++)
+ {
+ if(image[i,j] == 0) Console.Write($" ");
+ else Console.Write($"+");
+ }
+ Console.WriteLine();
+ }
+}
+void FillImage(int row, int col)
+{
+ if (pic[row, col] == 0)
+ {
+ pic[row, col] = 1;
+ FillImage(row - 1, col);
+ FillImage(row, col - 1);
+ FillImage(row + 1, col);
+ FillImage(row, col + 1);
+ }
+}
+PrintImage(pic); 
+FillImage(13, 13);
