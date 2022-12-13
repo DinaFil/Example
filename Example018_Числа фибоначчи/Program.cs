72761d1db3cd95c8e7d4d7965a7077d93c0c1548
Dina Filippova <dinafilippova@gmail.com>
2022-12-13 14:15:02 +0700

    Фибоначчи

---
+++ Example018_Числа фибоначчи/Program.cs
@@ -0,0 +1,8 @@
+﻿double Fibonacci(int n)
+{
+if (n == 2 || n == 1) return 1;
+else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+Console.WriteLine($"{Fibonacci(i)} = {i}");
